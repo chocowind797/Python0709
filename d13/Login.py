229141e91f07627cb5d16bf32b419949899d7434
chocowind797 <ziyun5257055@gmail.com>
2020-08-13 19:25:20 +0800

    Python

--- d13/Login.py
+++ d13/Login.py
@@ -1,3 +1,11 @@
+class LoginException(Exception):
+    def __init__(self, msg) -> None:
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return "登入錯誤, 原因: " + self.msg
+
+
 users = [{'name': "john", 'password': '4321'},
          {'name': "admin", 'password': '1234'}
          ]
@@ -11,16 +19,16 @@ def loginCheck(u, p):
             if user['password'] == p:
                 check[1] = True
     if not check[0]:
-        raise Exception("查無此帳號")
+        raise LoginException("無此帳號")
     if not check[1]:
-        raise Exception("密碼錯誤")
+        raise LoginException("密碼錯誤")
     return "登入成功"
 
 
 if __name__ == '__main__':
     try:
         result = loginCheck('admin', '1234')
-    except Exception as e:
-        print(e)
+    except LoginException as le:
+        print(le)
     else:
         print(result)
