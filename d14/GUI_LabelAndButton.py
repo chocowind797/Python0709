4f1c50540d5717029deafc6117d7770b9c72872d
chocowind797 <ziyun5257055@gmail.com>
2020-08-13 20:49:48 +0800

    Python GUI

--- d14/GUI_LabelAndButton.py
+++ d14/GUI_LabelAndButton.py
@@ -1,4 +1,14 @@
 import tkinter as tk
+from tkinter import messagebox
+
+
+def hello():
+    messagebox.showinfo('Hello', 'Python')
+
+
+def cancel():
+    win.quit()
+
 
 win = tk.Tk()
 
@@ -6,12 +16,15 @@ win.title("我的小視窗")
 win.geometry("300x250")
 
 label = tk.Label(win, text="Hello!")
+label.config(font=('Arial', 40), bg='red', fg='yellow')
 label.pack()
 
-button1 = tk.Button(win, text='OK')
+button1 = tk.Button(win, text='OK', command=hello)
+button1.config(font=("Arial", 30))
 button1.pack(side=tk.LEFT)
 
-button2 = tk.Button(win, text='Exit')
+button2 = tk.Button(win, text='Exit', command=cancel)
+button2.config(font=("Arial", 30))
 button2.pack(side=tk.RIGHT)
 
 win.mainloop()
