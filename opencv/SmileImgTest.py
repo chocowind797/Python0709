c028d7a182b206ddfd3f896a1f7449aa4f81e373
BuildTools <unconfigured@null.spigotmc.org>
2020-08-15 12:52:19 +0800

    Python OpenCV

--- opencv/SmileImgTest.py
+++ opencv/SmileImgTest.py
@@ -1,51 +1,52 @@
+# -*- coding=utf-8 -*-
 import cv2
-import os
 
-# 匯入辨識檔
-face_cascade = cv2.CascadeClassifier('./xml/haarcascade_frontalface_alt.xml')
-smile_cascade = cv2.CascadeClassifier('./xml/haarcascade_smile.xml')
+facePath = "./xml/haarcascade_frontalface_alt.xml"
+faceCascade = cv2.CascadeClassifier(facePath)
 
-img = '2'
-frame = cv2.imread('./image/%s.jpg' % img)
+smilePath = "./xml/haarcascade_smile.xml"
+smileCascade = cv2.CascadeClassifier(smilePath)
 
-# 取得灰階圖樣
-gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+image = '3'
+img = cv2.imread("./image/%s.jpg" % image)
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-# 偵測笑容
-smile = smile_cascade.detectMultiScale(
-    gray,  # 待檢測圖片，一般為灰度圖像加快檢測速度
-    scaleFactor=1.1,  # 檢測粒度 scaleFactor 。更大的粒度將會加快檢測的速度，但是會對檢測準確性產生影響。相反的，一個更小的粒度將會影響檢測的時間，但是會增加準確性。
-    minNeighbors=5,  # 每個目標至少檢測到幾次以上，才可被認定是真數據(愈大愈準,但速度愈慢)。
-    minSize=(30, 30),  # 設定數據搜尋的最小尺寸 ，如 minSize=(30,30)
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=8,
+    minSize=(30, 30),
     flags=cv2.CASCADE_SCALE_IMAGE
 )
 
-faces = face_cascade.detectMultiScale(
-    gray,  # 待檢測圖片，一般為灰度圖像加快檢測速度
-    scaleFactor=1.1,  # 檢測粒度 scaleFactor 。更大的粒度將會加快檢測的速度，但是會對檢測準確性產生影響。相反的，一個更小的粒度將會影響檢測的時間，但是會增加準確性。
-    minNeighbors=15,  # 每個目標至少檢測到幾次以上，才可被認定是真數據(愈大愈準,但速度愈慢)。
-    minSize=(30, 30),  # 設定數據搜尋的最小尺寸 ，如 minSize=(30,30)
-    flags=cv2.CASCADE_SCALE_IMAGE
-)
+# 畫出每一個臉
+for (x, y, w, h) in faces:
+    # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
 
-print(smile)
+    # 對人臉進行微笑偵測
+    smile = smileCascade.detectMultiScale(
+        roi_gray,
+        scaleFactor= 1.16,
+        minNeighbors=35,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
 
-# 在嘴部四周繪製方形
-for (fx, fy, fw, fh) in faces:
-    for (sx, sy, sw, sh) in smile:
-        if fx + fw > sx > fx and fy + fh > sy > fy:
-            cv2.putText(frame, 'smile', (sx, sy-7), 2, 1.2, (255, 0, 0), 2)
-            cv2.rectangle(frame, (sx, sy), (sx+sw, sy+sh), (255, 0, 0), 2)
-        else:
-            cv2.putText(frame, 'no smile', (0, 23), 2, 0.8, (0, 0, 255), 1)
+    # 框出上揚嘴角，並打上 Smile 標籤
+    print(smile)
+    for (x2, y2, w2, h2) in smile:
+        cv2.rectangle(roi_color, (x2, y2), (x2+w2, y2+h2), (255, 0, 0), 2)
+        cv2.putText(img, 'Smile', (x+x2, y+y2-7), 3, 1.2, (0, 255, 0), 2)
 
-# 顯示影像
-cv2.imshow('OpenCV', frame)
+    if len(smile) == 0:
+        cv2.putText(img, 'No Smile', (x, y - 7), 3, 1.2, (0, 0, 255), 2)
 
-if not os.path.exists('./result'):
-    os.mkdir('./result')
-cv2.imwrite('./result/%s.jpg' % img, frame)
+cv2.imshow('Smile', img)
 
-cv2.waitKey(0)
+# 儲存檔案
+cv2.imwrite("./result/%s.jpg" % image, img)
 
-cv2.destroyAllWindows()
+# 任意鍵離開(若設定為 0 就表示持續等待至使用者按下按鍵為止)
+c = cv2.waitKey(0)
