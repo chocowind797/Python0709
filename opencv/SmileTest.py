e0ca7a16ddefe8e55d9a7e0627473f2cb83b201b
BuildTools <unconfigured@null.spigotmc.org>
2020-08-15 12:00:30 +0800

    Python OpenCV

--- opencv/SmileTest.py
+++ opencv/SmileTest.py
@@ -3,6 +3,7 @@ import cv2
 # 匯入辨識檔
 face_cascade = cv2.CascadeClassifier('./xml/haarcascade_frontalface_alt.xml')
 smile_cascade = cv2.CascadeClassifier('./xml/haarcascade_smile.xml')
+eyes_cascade = cv2.CascadeClassifier('./xml/haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -18,7 +19,7 @@ while True:
     smile = smile_cascade.detectMultiScale(
         gray,  # 待檢測圖片，一般為灰度圖像加快檢測速度
         scaleFactor=1.1,  # 檢測粒度 scaleFactor 。更大的粒度將會加快檢測的速度，但是會對檢測準確性產生影響。相反的，一個更小的粒度將會影響檢測的時間，但是會增加準確性。
-        minNeighbors=300,  # 每個目標至少檢測到幾次以上，才可被認定是真數據(愈大愈準,但速度愈慢)。
+        minNeighbors=250,  # 每個目標至少檢測到幾次以上，才可被認定是真數據(愈大愈準,但速度愈慢)。
         minSize=(30, 30),  # 設定數據搜尋的最小尺寸 ，如 minSize=(30,30)
         flags=cv2.CASCADE_SCALE_IMAGE
     )
@@ -31,13 +32,23 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
+    eyes = eyes_cascade.detectMultiScale(
+        gray,  # 待檢測圖片，一般為灰度圖像加快檢測速度
+        scaleFactor=1.1,  # 檢測粒度 scaleFactor 。更大的粒度將會加快檢測的速度，但是會對檢測準確性產生影響。相反的，一個更小的粒度將會影響檢測的時間，但是會增加準確性。
+        minNeighbors=5,  # 每個目標至少檢測到幾次以上，才可被認定是真數據(愈大愈準,但速度愈慢)。
+        minSize=(30, 30),  # 設定數據搜尋的最小尺寸 ，如 minSize=(30,30)
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
     print(smile)
 
     # 在嘴部四周繪製方形
-    for (x, y, w, h) in faces:  # B  G  R  線的寬
-        for (x2, y2, w2, h2) in smile:
-            if x + w > x2 > x and y + h > y2 > y:              # B  G  R  線的寬
-                cv2.rectangle(frame, (x2, y2), (x2+w2, y2+h2), (255, 0, 0), 5)
+    for (fx, fy, fw, fh) in faces:
+        cv2.rectangle(frame, (fx, fy), (fx + fw, fy + fh), (0, 255, 0), 5)
+        cv2.putText(frame, 'Test', (fx, fy-7), 2, 1.2, (0, 255, 0), 2)
+        for (sx, sy, sw, sh) in smile:
+            if fx + fw > sx > fx and fy + fh > sy > fy:
+                cv2.rectangle(frame, (sx, sy), (sx+sw, sy+sh), (255, 0, 0), 5)
 
     # 顯示影像
     cv2.imshow('OpenCV', frame)
