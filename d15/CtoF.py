9820e5c3519efc760e145dbe18cbd1f93087fcec
BuildTools <unconfigured@null.spigotmc.org>
2020-08-20 20:32:02 +0800

    Python GUI

--- d15/CtoF.py
+++ d15/CtoF.py
@@ -2,32 +2,22 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.messagebox as msg
 
-global string_c
-global string_f
-global win
-
 
 def ctof():
     try:
         c = string_c.get()
-        if c == '':
-            c = 0
-            string_c.set("0.0")
         c = "%.1f" % (c * 9 / 5 + 32)
         string_f.set(c)
-    except ValueError:
+    except (ValueError, tk.TclError):
         msg.showerror("輸入錯誤", "請輸入數字")
 
 
 def ftoc():
     try:
         f = string_f.get()
-        if f == '':
-            f = 0
-            string_f.set("0.0")
         f = "%.1f" % ((f - 32) * 5 / 9)
         string_c.set(f)
-    except ValueError:
+    except (ValueError, tk.TclError):
         msg.showerror("輸入錯誤", "請輸入數字")
 
 
@@ -51,7 +41,7 @@ if __name__ == '__main__':
     # 攝氏
     label_c = ttk.Label(frame_c, text='攝氏', font=('Arial', 20))
 
-    string_c = tk.DoubleVar().set("")
+    string_c = tk.DoubleVar()
     entry_c = tk.Entry(frame_c, textvariable=string_c, justify="center")
 
     button_c = ttk.Button(frame_c, text='轉換', style="My.TButton", command=ctof)
@@ -63,7 +53,7 @@ if __name__ == '__main__':
     # 華氏
     label_f = ttk.Label(frame_f, text='華氏', font=('Arial', 20))
 
-    string_f = tk.DoubleVar().set("")
+    string_f = tk.DoubleVar()
     entry_f = tk.Entry(frame_f, textvariable=string_f, justify="center")
 
     button_f = ttk.Button(frame_f, text='轉換', style="My.TButton", command=ftoc)
