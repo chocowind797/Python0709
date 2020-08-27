b8fc1e3491590f4bea8755663aebd4278c128dc0
BuildTools <unconfigured@null.spigotmc.org>
2020-08-27 19:59:23 +0800

    Python SQL

--- d18/SelectStudent.py
+++ d18/SelectStudent.py
@@ -3,6 +3,9 @@ import sqlite3
 conn = sqlite3.connect('student.db')
 cursor = conn.cursor()
 
+print('序號 ', '身高\t', '體重\t\t', 'BMI\t\t', '姓名\t', sep='')
+print("===================================")
+
 sql = 'SELECT b.id, b.h, b.w, round(b.w/((b.h/100)*(b.h/100)), 2) as bmi, s.name ' \
       'FROM student as s, bmi as b ' \
       'WHERE s.id = b.sid ' \
